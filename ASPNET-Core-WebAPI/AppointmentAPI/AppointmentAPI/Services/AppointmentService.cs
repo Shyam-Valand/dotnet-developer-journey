71be35ebae5098bee0d5ed26c2cfcc8b938a7e21
Shyam Valand <shyamvaland213@gmail.com>
2026-06-27 16:36:52 +0530

    Implement patient appointment ownership and authorization

--- ASPNET-Core-WebAPI/AppointmentAPI/AppointmentAPI/Services/AppointmentService.cs
+++ ASPNET-Core-WebAPI/AppointmentAPI/AppointmentAPI/Services/AppointmentService.cs
@@ -1,155 +1,181 @@
-﻿    using AppointmentAPI.DTOs;
-    using AppointmentAPI.Models;
-    using AppointmentAPI.Repositories;
-    using AppointmentAPI.Exceptions;
+﻿using AppointmentAPI.DTOs;
+using AppointmentAPI.Models;
+using AppointmentAPI.Repositories;
+using AppointmentAPI.Exceptions;
 
-    namespace AppointmentAPI.Services;
+namespace AppointmentAPI.Services;
 
-    public class AppointmentService : IAppointmentService
+public class AppointmentService : IAppointmentService
+{
+    private readonly IAppointmentRepository _repository;
+    private readonly ICurrentUserService _currentUserService;
+
+    public AppointmentService(IAppointmentRepository repository, ICurrentUserService currentUserService)
     {
-        private readonly IAppointmentRepository _repository;
-        private readonly ICurrentUserService _currentUserService;
-
-        public AppointmentService(IAppointmentRepository repository,ICurrentUserService currentUserService)
-        {
-            _repository = repository;
-            _currentUserService = currentUserService;
-        }
+        _repository = repository;
+        _currentUserService = currentUserService;
+    }
 
     public async Task<List<AppointmentDto>> GetAllAppointmentsAsync()
-        {
+    {
         var appointments = await _repository.GetByUserIdAsync(_currentUserService.UserId);
 
         return appointments.Select(x => new AppointmentDto
-            {
-                Id = x.Id,
-                CustomerName = x.Customer!.Name,
-                ServiceName = x.Service!.Name,
-                AppointmentDate = x.AppointmentDate,
-                Status = x.Status
-            }).ToList();
-        }
-
-        public async Task<AppointmentDto?> GetAppointmentByIdAsync(int id)
         {
-            var appointment = await _repository.GetByIdAsync(id);
-
-            if (appointment == null)
-                return null;
-
-            return new AppointmentDto
-            {
-                Id = appointment.Id,
-                CustomerName = appointment.Customer!.Name,
-                ServiceName = appointment.Service!.Name,
-                AppointmentDate = appointment.AppointmentDate,
-                Status = appointment.Status
-            };
-        }
-
-        public async Task<AppointmentDto> CreateAppointmentAsync(CreateAppointmentDto dto)
-        {
-            if (dto.AppointmentDate < DateTime.Now)
-            {
-                throw new BadRequestException("Appointment date cannot be in the past");
-            }
-
-            var serviceDuration = await _repository.GetServiceDurationAsync(dto.ServiceId);
-            var appointmentEndTime = dto.AppointmentDate.AddMinutes(serviceDuration);
-            bool overlap = await _repository.HasOverlappingAppointmentAsync(
-                    dto.ServiceId,
-                    dto.AppointmentDate,
-                    appointmentEndTime
-                );
-
-            if (overlap)
-            {
-                throw new BadRequestException(
-                    "Appointment time overlaps with existing booking"
-                );
-            }
-
-            var appointment = new Appointment
-            {
-                UserId = _currentUserService.UserId,
-                CustomerId = dto.CustomerId,
-                ServiceId = dto.ServiceId,
-                AppointmentDate = dto.AppointmentDate,
-                Status = "Booked"
-            };
-
-            await _repository.AddAsync(appointment);
-            await _repository.SaveAsync();
-
-            var createdAppointment = await _repository.GetByIdAsync(appointment.Id);
-
-            return new AppointmentDto
-            {
-                Id = createdAppointment!.Id,
-                CustomerName = createdAppointment.Customer!.Name,
-                ServiceName = createdAppointment.Service!.Name,
-                AppointmentDate = createdAppointment.AppointmentDate,
-                Status = createdAppointment.Status
-            };
-        }
-
-        public async Task<AppointmentDto?> UpdateAppointmentAsync(int id,UpdateAppointmentDto dto)
-        {
-            var appointment = await _repository.GetByIdAsync(id);
-
-            if (appointment == null)
-            {
-                throw new NotFoundException(
-                    "Appointment not found"
-                );
-            }
-
-            if (dto.AppointmentDate < DateTime.Now)
-            {
-                throw new BadRequestException(
-                    "Appointment date cannot be in the past"
-                );
-            }
-
-            var serviceDuration = await _repository.GetServiceDurationAsync(appointment.ServiceId);
-
-            var appointmentEndTime = dto.AppointmentDate.AddMinutes(serviceDuration);
-            bool overlap = await _repository.HasOverlappingAppointmentAsync(
-                    appointment.ServiceId,
-                    dto.AppointmentDate,
-                    appointmentEndTime,
-                    appointment.Id
-                );
-
-            if (overlap)
-            {
-                throw new BadRequestException(
-                    "Appointment time overlaps with existing booking"
-                );
-            }
-            appointment.AppointmentDate = dto.AppointmentDate;
-            appointment.Status = dto.Status;
-            await _repository.SaveAsync();
-
-            return new AppointmentDto
-            {
-                Id = appointment.Id,
-                CustomerName = appointment.Customer!.Name,
-                ServiceName = appointment.Service!.Name,
-                AppointmentDate = appointment.AppointmentDate,
-                Status = appointment.Status
-            };
-        }
-
-        public async Task<bool> DeleteAppointmentAsync(int id)
-        {
-            var appointment = await _repository.GetByIdAsync(id);
-            if (appointment == null)
-                return false;
-
-            _repository.Delete(appointment);
-            await _repository.SaveAsync();
-
-            return true;
-        }
+            Id = x.Id,
+            CustomerName = x.Customer!.Name,
+            ServiceName = x.Service!.Name,
+            AppointmentDate = x.AppointmentDate,
+            Status = x.Status
+        }).ToList();
     }
+
+    public async Task<AppointmentDto?> GetAppointmentByIdAsync(int id)
+    {
+        var appointment = await _repository.GetByIdAsync(id);
+
+        if (appointment == null)
+        {
+            throw new NotFoundException("Appointment not found");
+        }
+
+        if (appointment.UserId != _currentUserService.UserId)
+        {
+            throw new BadRequestException("You are not authorized to access this appointment");
+        }
+
+        return new AppointmentDto
+        {
+            Id = appointment.Id,
+            CustomerName = appointment.Customer!.Name,
+            ServiceName = appointment.Service!.Name,
+            AppointmentDate = appointment.AppointmentDate,
+            Status = appointment.Status
+        };
+    }
+
+    public async Task<AppointmentDto> CreateAppointmentAsync(CreateAppointmentDto dto)
+    {
+        if (dto.AppointmentDate < DateTime.Now)
+        {
+            throw new BadRequestException("Appointment date cannot be in the past");
+        }
+
+        var serviceDuration = await _repository.GetServiceDurationAsync(dto.ServiceId);
+        var appointmentEndTime = dto.AppointmentDate.AddMinutes(serviceDuration);
+        bool overlap = await _repository.HasOverlappingAppointmentAsync(
+                dto.ServiceId,
+                dto.AppointmentDate,
+                appointmentEndTime
+            );
+
+        if (overlap)
+        {
+            throw new BadRequestException(
+                "Appointment time overlaps with existing booking"
+            );
+        }
+
+        var appointment = new Appointment
+        {
+            UserId = _currentUserService.UserId,
+            CustomerId = dto.CustomerId,
+            ServiceId = dto.ServiceId,
+            AppointmentDate = dto.AppointmentDate,
+            Status = "Booked"
+        };
+
+        await _repository.AddAsync(appointment);
+        await _repository.SaveAsync();
+
+        var createdAppointment = await _repository.GetByIdAsync(appointment.Id);
+
+        return new AppointmentDto
+        {
+            Id = createdAppointment!.Id,
+            CustomerName = createdAppointment.Customer!.Name,
+            ServiceName = createdAppointment.Service!.Name,
+            AppointmentDate = createdAppointment.AppointmentDate,
+            Status = createdAppointment.Status
+        };
+    }
+
+    public async Task<AppointmentDto?> UpdateAppointmentAsync(int id, UpdateAppointmentDto dto)
+    {
+        var appointment = await _repository.GetByIdAsync(id);
+
+        if (appointment == null)
+        {
+            throw new NotFoundException(
+                "Appointment not found"
+            );
+        }
+
+        if (appointment.UserId != _currentUserService.UserId)
+        {
+            throw new BadRequestException(
+                "You are not authorized to update this appointment"
+            );
+        }
+
+        if (dto.AppointmentDate < DateTime.Now)
+        {
+            throw new BadRequestException(
+                "Appointment date cannot be in the past"
+            );
+        }
+
+        var serviceDuration = await _repository.GetServiceDurationAsync(appointment.ServiceId);
+
+        var appointmentEndTime = dto.AppointmentDate.AddMinutes(serviceDuration);
+        bool overlap = await _repository.HasOverlappingAppointmentAsync(
+                appointment.ServiceId,
+                dto.AppointmentDate,
+                appointmentEndTime,
+                appointment.Id
+            );
+
+        if (overlap)
+        {
+            throw new BadRequestException(
+                "Appointment time overlaps with existing booking"
+            );
+        }
+        appointment.AppointmentDate = dto.AppointmentDate;
+        appointment.Status = dto.Status;
+        await _repository.SaveAsync();
+
+        return new AppointmentDto
+        {
+            Id = appointment.Id,
+            CustomerName = appointment.Customer!.Name,
+            ServiceName = appointment.Service!.Name,
+            AppointmentDate = appointment.AppointmentDate,
+            Status = appointment.Status
+        };
+    }
+
+    public async Task<bool> DeleteAppointmentAsync(int id)
+    {
+        var appointment = await _repository.GetByIdAsync(id);
+
+        if (appointment == null)
+        {
+            throw new NotFoundException(
+                "Appointment not found"
+            );
+        }
+
+        if (appointment.UserId != _currentUserService.UserId)
+        {
+            throw new BadRequestException(
+                "You are not authorized to delete this appointment"
+            );
+        }
+
+        _repository.Delete(appointment);
+        await _repository.SaveAsync();
+
+        return true;
+    }
+}
